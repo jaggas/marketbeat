d3146235ca65d3783e767c941c950b9536154235
Jeff Aggas <jeffrey.aggas@gmail.com>
2021-10-30 13:12:34 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='marketbeat',
+    version='1.0',
+
+    url='https://github.com/jaggas/marketbeat.git',
+    author='Jeff Aggas',
+    author_email='jeffrey.aggas@gmail.com',
+
+    py_modules=['marketbeat'],
+)
